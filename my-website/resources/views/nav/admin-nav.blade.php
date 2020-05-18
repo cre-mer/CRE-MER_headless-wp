9f2c6c6ea5bc0a1e61b0cc81f8cd1c23fe1f7c9e
cre-mer <jonasmerhej1@gmail.com>
2020-05-18 22:01:10 +0200

    Edit admin navbar responsiveness

--- my-website/resources/views/nav/admin-nav.blade.php
+++ my-website/resources/views/nav/admin-nav.blade.php
@@ -7,7 +7,7 @@
     <nav class="admin-nav">
         <ul class="admin-nav_list admin-nav_dashboard">
             <li><a href="/" aria-label="{{ __('home') }}"><img alt="{{ __('home') }}" title="{{ __('home') }}" src="{{ URL::asset('images/home.svg') }}"/></a></li>
-            <li><a href="{{ route('wp-admin') }}" target="_blank">{{env('APP_NAME')}} dashboard</a></li>
+            <li><a href="{{ route('wp-admin') }}" target="_blank">{{ env('APP_NAME', 'Website') }} dashboard</a></li>
         </ul>
 
         <ul class="admin-nav_list">
