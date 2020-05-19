0572b08693ec09825fdad85d0d46446da4928fe6
cre-mer <jonasmerhej1@gmail.com>
2020-05-19 03:58:17 +0200

    Fix title

--- my-website/resources/views/app.blade.php
+++ my-website/resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ env('APP_NAME') }} - @yield('title')</title>
+        <title>{{ env('APP_NAME') }} - @yield('title', env('APP_NAME'))</title>
         <meta name="og:title" content="{{ $post['seo_title'] ?? '' }}">
         <meta name="description" content="{{ $post['meta_description'] ?? '' }}">
 
@@ -27,7 +27,7 @@
         <!-- Scripts -->
         <script type="text/javascript">
             const CSRF = "{{ csrf_token() }}";
-            const ACTION_URL = "https://api.my-website.test/wp-json/wp/v2/comments";
+            const ACTION_URL = env('WP_API_BASE_URL') . "/wp-json/wp/v2/comments";
         </script>
         <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
         @stack('scripts')
--- my-website/resources/views/welcome.blade.php
+++ my-website/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @extends('app')
 
-@section('title', $post['title'] ?? '')
+@section('title', $post['title'] ?? null)
 
 <!-- Styles -->
 @push('styles')
